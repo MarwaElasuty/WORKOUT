993b68b6b894447ffebd2c851a0ae3f532800954
Marwa Mohammed <52058731+MarwaElasuty@users.noreply.github.com>
2024-01-30 09:11:36 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var toggle = document.querySelector(".ul-div");
+       function appear(){
+        if(toggle.style.display=='block'){
+          toggle.style.display='none';
+        }
+        else{
+          toggle.style.display='block';
+        }
+       }
